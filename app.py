577396fea73c8bfaa6c93ac2130e7379063fa511
zonglianghou@163.com <EnderZ@sjtu.edu.cn>
2026-02-05 22:54:16 +0800

    refactor: delete unused import modules

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode, JsCode
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 
 # Configure page (centered layout, not wide)
 st.set_page_config(
